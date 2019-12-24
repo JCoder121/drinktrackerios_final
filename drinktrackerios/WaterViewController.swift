57d4f7086c44d2658b9b7aa15925b8ea48d7e9ac
Jeffrey Chen <j.chen14641@gmail.com>
2019-12-23 22:29:56 -0800

    finished

--- drinktrackerios/WaterViewController.swift
+++ drinktrackerios/WaterViewController.swift
@@ -11,18 +11,15 @@ import UIKit
 class WaterViewController: UIViewController {
     
     //SET DRINKING WATER GOAL FOR 128 oz
-    
     var goal: Double = 128
     
     @IBOutlet weak var displayLabel: UILabel!
-           
     @IBOutlet weak var textField: UITextField!
-       
-       
     @IBOutlet weak var waterprogress: UIProgressView!
-       
     @IBOutlet weak var addButton: UIButton!
-
+    @IBOutlet weak var undoButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.keyboardType = UIKeyboardType.numberPad
@@ -41,22 +38,13 @@ class WaterViewController: UIViewController {
         waterprogress.layer.sublayers![1].cornerRadius = 10
         waterprogress.subviews[1].clipsToBounds = true
             
-        
- 
-   
         }
- 
-    
     }
     
     var final_water: Double = 0
-    
     var toAdd: Double = 0
     var calculate_progress: Double = 0
     
-
-   
-
     @IBAction func saveButtonClick(sender: AnyObject) {
         
         self.view.endEditing(true)
@@ -78,39 +66,37 @@ class WaterViewController: UIViewController {
             
         }
         
-      
     }
     
     func isStringAnInt(string: String) -> Bool {
         return Int(string) != nil
     }
     
-    @IBAction func undoButton(_ sender: Any) {
+
+    @IBAction func undo(_ sender: Any) {
+        
         final_water -= toAdd
         displayLabel.text! = String(final_water)
         _ = UserDefaults.standard.set(displayLabel.text, forKey: "name")
-        
+
         waterprogress.progress = Float(final_water/goal)
+
         
     }
     
-    
-    @IBAction func resetButton(_ sender: Any) {
-        
-        final_water = 0
+    @IBAction func reset(_ sender: Any) {        final_water = 0
         displayLabel.text = String(final_water)
         _ = UserDefaults.standard.set(displayLabel.text, forKey: "name")
         
         waterprogress.progress = 0
+        
     }
     
+
+
+   
     func textFieldDidBeginEditing(textField: UITextField) {
         textField.text = ""
     }
         
 }
-
-
-
-
-
--- drinktrackerios/gif_info/GIFViewController.swift
+++ drinktrackerios/gif_info/GIFViewController.swift
@@ -11,33 +11,15 @@ import FLAnimatedImage
 
 class GIFViewController: UIViewController {
 
-    
     @IBOutlet weak var waterfall: FLAnimatedImageView!
-    
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
-                let imageData = try! Data(contentsOf: Bundle.main.url(forResource: "true_test", withExtension: "gif")!)
+        let imageData = try! Data(contentsOf: Bundle.main.url(forResource: "true_test", withExtension: "gif")!)
         
         waterfall.animatedImage = FLAnimatedImage(animatedGIFData: imageData)
         
     }
-    
-    
-    
-   
-   
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
